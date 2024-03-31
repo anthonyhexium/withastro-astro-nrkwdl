d847def9d9042992e5f0e27aedb568250a58ddd9
anthonyhexium <anthony@hexium.io>
2024-03-31 19:12:21 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    domains: ['a.storyblok.com']
+  },
+  output: "server",
+  adapter: vercel({
+    // COMMENTED DURING DEV
+    // imageService: true,
+    // imagesConfig: {
+    //   sizes: [384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    //   formats: ["image/avif", "image/webp"],
+    //   domains: ['a.storyblok.com'],
+    // },
+    // JUST A TRY
+    // devImageService: 'sharp',
+  })
+});
